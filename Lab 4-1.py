b12f57e6d772f9119c6ab604c0f8571ef1ce142d
JNeaderland <144148081+JNeaderland@users.noreply.github.com>
2023-10-05 09:02:22 -0400

    Add files via upload

---
+++ Lab 4-1.py
@@ -0,0 +1,14 @@
+#Author:Jacob Neaderland
+#Stores First Name
+namef = input ("What is your first name? ")
+
+#Stores Last Name
+namel = input("What is your last name? ")
+
+#COmbines first and last name
+name = namef + namel
+print (name)
+
+#No space between first and last name, could be fixed by inserting a space in quotes character
+name = namef + " " + namel
+print (name)
